d484ec2bb88d253b64b402b5779eccd79d272540
shall <zyydqpi@163.com>
2016-11-02 20:57:09 -0700

    add param junit tese example

---
+++ src/test/java/example/PrimeNumberChecker.java
@@ -0,0 +1,14 @@
+package junittest.example;
+
+
+public class PrimeNumberChecker {
+    public Boolean validate(final Integer primeNumber) {
+        for (int i = 2; i < primeNumber; i++) {
+            if (primeNumber % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
